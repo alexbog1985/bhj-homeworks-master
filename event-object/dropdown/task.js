9262ba5d7cf91414c5e9a4e84c3424be7c34b725
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-12-20 19:22:08 +0300

    dropdown complete

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,6 +1,18 @@
-const dropdown = document.querySelector(".dropdown__value");
-const dropdown__list = document.querySelector(".dropdown__list");
+const btn = document.querySelector(".dropdown__value");
+const dropdown = document.querySelector(".dropdown__list");
 
-dropdown.addEventListener("click", () => {
-  dropdown__list.classList.toggle("dropdown__list_active");
+const dropdownList = Array.from(document.querySelectorAll(".dropdown__item"));
+console.log(dropdownList);
+console.log(btn.textContent);
+
+dropdownList.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    dropdown.classList.remove("dropdown__list_active");
+    btn.textContent = item.textContent;
+    event.preventDefault();
+  });
+});
+
+btn.addEventListener("click", () => {
+  dropdown.classList.toggle("dropdown__list_active");
 });
